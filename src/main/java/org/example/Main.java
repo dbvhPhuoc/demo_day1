c9aa28ecb8ce452a8acfcfd61c4214471ee55533
dbvhPhuoc <147008949+dbvhPhuoc@users.noreply.github.com>
2024-08-02 19:55:10 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello HOAI THUONG!");
+        System.out.println("Hello HUU PHUOC!");
     }
 }
 //test commit
